ba5ee3395f0415b08863e4351dfc150fb588d761
Fedor <andryushin.fedor@gmail.com>
2021-12-14 18:01:34 +0300

    upd

--- js/indexBlack.js
+++ js/indexBlack.js
@@ -4,10 +4,79 @@ window.onload = function(){
 const menuButtonBlack = document.querySelector('.menu-buttonBlack');
 const menuOverlayBlack = document.querySelector('.menu-overlayBlack');
 
+// cursor 
 
 menuButtonBlack.addEventListener('click', function() {
     menuButtonBlack.classList.toggle('active');
     menuOverlayBlack.classList.toggle('open');
 }); 
-
- }
+    let mouseCursor = document.querySelector('.cursor');
+  
+    window.addEventListener('mousemove',cursor);
+  
+    function cursor(e) {
+      mouseCursor.style.top = e.pageY + 'px';
+      mouseCursor.style.left = e.pageX + 'px';
+  
+    }
+    $(function () {
+    $("#testarea").mousemove(function (e) {
+      $(".cursor").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor").hide();
+    });
+  });
+  $(function () {
+    $("#testarea2").mousemove(function (e) {
+      $(".cursor2").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor2").hide();
+    });
+  });
+  $(function () {
+    $("#testarea2").mousemove(function (e) {
+      $(".cursor2").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor2").hide();
+    });
+  });
+  $(function () {
+    $("#testarea3").mousemove(function (e) {
+      $(".cursor2").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor2").hide();
+    });
+  });
+  $(function () {
+    $("#testarea4").mousemove(function (e) {
+      $(".cursor2").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor2").hide();
+    });
+  });
+  $(function () {
+    $("#testarea5").mousemove(function (e) {
+      $(".cursor2").show().css({
+        "left": e.clientX,
+        "top": e.clientY
+      });
+    }).mouseout(function () {
+      $(".cursor2").hide();
+    });
+  });
+  };

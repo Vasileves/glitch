b1e0c322d5da0d08674625f49d796ece83aeabf6
Fedor <andryushin.fedor@gmail.com>
2022-04-14 18:52:25 +0300

    upd

--- js/indexBlack.js
+++ js/indexBlack.js
@@ -33,8 +33,7 @@ if( window.innerWidth > 600 ) {
   });
 });
 $(function () {
-  $("#testarea2")
-  .mousemove(function (e) {
+  $("#testarea2").mousemove(function (e) {
     $(".cursor2").show().css({
       "left": e.clientX,
       "top": e.clientY
--- js/unwrap.js
+++ js/unwrap.js
@@ -6,10 +6,10 @@
 
     //vimeo embed version
 
-    //x.innerHTML = ` <img src="images/close.svg" class= "player-close"/> <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="${vidLink}" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>";`
+    x.innerHTML = ` <img src="images/close.svg" class= "player-close"/> <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="${vidLink}" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>";`
 
-    //native version (new)
-    x.innerHTML = ` <img src="images/close.svg" class= "player-close" onclick="unwrapPlayer(this)" /> <video width='100%' height='100%' controls autoplay>   <source src="${vidLink}" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></video> ";`
+    //native <video> version (new )
+    //x.innerHTML = ` <img src="images/close.svg" class= "player-close"/> <video width='100%' height='100%' controls autoplay>   <source src="${vidLink}" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></video> ";`
 
   } else {
     x.style.display = "none";
